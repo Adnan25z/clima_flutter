16411095ad99dc8fe5bcc7da407658661762b443
Adnan <adnanzafar2001@gmail.com>
2025-07-26 15:19:44 +1000

    Getting location data from across platforms using geolocator

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,13 +7,45 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  void getLocation() async {
+    LocationPermission permission;
+
+    // Handle permissions first
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        print('Location permissions are denied');
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      print('Location permissions are permanently denied');
+      return;
+    }
+
+    // New way using locationSettings
+    final settings = LocationSettings(
+      accuracy: LocationAccuracy.low,
+      distanceFilter: 0,
+      timeLimit: Duration(seconds: 10),
+    );
+
+    Position position = await Geolocator.getCurrentPosition(
+      locationSettings: settings,
+    );
+
+    print('Latitude: ${position.latitude}, Longitude: ${position.longitude}');
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            // Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
