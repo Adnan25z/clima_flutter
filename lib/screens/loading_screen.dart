57c77e71d6cb85b6a0c3cc1aee164acf498e0afb
Adnan <adnanzafar2001@gmail.com>
2025-07-29 15:51:25 +1000

    adding page navigation and spinkit package for loading screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,9 @@
+import 'package:clima_flutter/screens/location_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:clima_flutter/services/location.dart';
 import 'package:clima_flutter/services/networking.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = 'adf966088203312b721011e15a54cea7';
 class LoadingScreen extends StatefulWidget {
@@ -25,10 +28,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
     NetworkHelper networkHelper = new NetworkHelper('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
     var weatherData = await networkHelper.getData();
+    Navigator.push(context,MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
