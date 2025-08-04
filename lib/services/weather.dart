df8b0588674aaced95f159653ac6d2d5ff60c488
Adnan <adnanzafar2001@gmail.com>
2025-08-04 16:37:37 +1000

    Passing data backwards through navigation stack

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -4,6 +4,14 @@ import 'package:clima_flutter/services/networking.dart';
 const apiKey = 'adf966088203312b721011e15a54cea7';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 class WeatherModel {
+
+  Future<dynamic> getCityWeather(String cityName) async {
+    var url = '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper = NetworkHelper(url);
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async{
     Location location = Location();
     await location.getCurrentLocation();
